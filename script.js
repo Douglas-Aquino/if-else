71792a84d591fae258a3e66c1f2783d1a419d0a5
Douglas <douglinhauniverso@gmail.com>
2022-01-20 17:56:50 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+//1 - Crie uma condição que exiba uma mensagem no console se a idade for maior que 18
+
+let idade = 16
+
+if(idade >=18){
+    console.log("Voçê é maoir de idade")
+}else{
+    console.log("Voçê é muleke")
+}
+
+//2 - Crie uma condição que exiba uma mensagem no console se você for maior de idade E a variável "humano" seja true
+
+let humano = true
+if(humano == true && idade >= 18){
+    console.log("As duas condições são verdadeiras")
+}else{
+    console.log("Uma das condições são falsas")
+}                          
+
+//3 - Criar uma condição que exiba uma mensagem no console se você fizer aniversário em Janeiro OU Dezembro
+
+let mes = "abril"
+if(mes == "janeiro" || mes == "dezembro"){
+    console.log("Voçê faz aniversário em um desses meses")
+}else{
+   console.log("Voçê não faz aniversário em dezembro muito menos em janeiro") 
+}
+
+//*** Exercícios bônus
+//4 - Criar uma condição que exiba uma mensagem no console se seu nome começar com a letra R
+
+//5 - Criar uma condição que exiba uma mensagem no console se seu sobrenome tenha mais de 6 letras OU seu nome começar com a letra E
